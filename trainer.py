e5067aaa023a6da95e2f998c13583ed82049ac1f
shi.feng <shi.feng@cs.ucla.edu>
2020-11-12 16:06:51 -0800

    Update trainer.py

--- trainer.py
+++ trainer.py
@@ -1,16 +1,16 @@
-import torch
-import torch.optim as optim
-import torch.nn.functional as fn
-from data.dataset import SkeletonDataset
-from torch_geometric.data import DataLoader
-from models.net import DualGraphTransformer
-from einops import rearrange
-from args import make_args
-from optimizer import get_std_opt
-from tensorboardX import SummaryWriter
-from tqdm import tqdm, trange
-import torch.nn as nn
 import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as fn
+from tensorboardX import SummaryWriter
+from torch_geometric.data import DataLoader
+from tqdm import tqdm
+
+from args import make_args
+from data.dataset import SkeletonDataset
+from models.net import DualGraphTransformer
+from optimizer import get_std_opt
+
 
 class GCNTrainer(object):
     def __init__(self, model, train_loader, train_labels, val_loader, val_labels, adj, optimizer, loss_fn, log_dir):
@@ -41,7 +41,7 @@ class GCNTrainer(object):
         self.model.train(True)
 
         for epoch in range(n_epochs):
-            #plot learning rate
+            # plot learning rate
             lr = self.optimizer.state_dict()['param_groups'][0]['lr']
             self.writer.add_scalar('params/lr', lr, epoch)
 
@@ -50,51 +50,51 @@ class GCNTrainer(object):
                 self.optimizer.zero_grad()
                 output = self.model(batch.x, adj=self.adj)
                 target = self.train_labels.gather(0, batch.batch)
-                #one_hot = fn.one_hot(target.long(), num_classes = 60)
+                # one_hot = fn.one_hot(target.long(), num_classes = 60)
 
-                #loss = fn.cross_entropy(output, one_hot)
+                # loss = fn.cross_entropy(output, one_hot)
                 loss = fn.cross_entropy(output, target.long())
-                self.writer.add_scalar('train/train_loss', loss, i_acc+i+1)
+                self.writer.add_scalar('train/train_loss', loss, i_acc + i + 1)
 
                 pred = torch.max(output, 1)[1]
                 results = pred == target
                 correct_points = torch.sum(results.long())
 
-                acc = correct_points.float()/results.size()[0]
-                self.writer.add_scalar('train/train_overall_acc', acc, i_acc+i+1)
+                acc = correct_points.float() / results.size()[0]
+                self.writer.add_scalar('train/train_overall_acc', acc, i_acc + i + 1)
 
                 loss.backward()
                 self.optimizer.step()
 
-                log_str = 'epoch %d, step %d: train_loss %.3f; train_acc %.3f' % (epoch+1, i+1, loss, acc)
-                if (i+1)%1==0:
+                log_str = 'epoch %d, step %d: train_loss %.3f; train_acc %.3f' % (epoch + 1, i + 1, loss, acc)
+                if (i + 1) % 1 == 0:
                     print(log_str)
             i_acc += i
 
-            #evaluation
-            if (epoch+1)%1==0:
+            # evaluation
+            if (epoch + 1) % 1 == 0:
                 with torch.no_grad():
-                    #loss, val_overall_acc, val_mean_class_acc = self.update_validation_accuracy()
+                    # loss, val_overall_acc, val_mean_class_acc = self.update_validation_accuracy()
                     loss, val_overall_acc = self.update_validation_accuracy()
-                #self.writer.add_scalar('val/val_mean_class_acc', val_mean_class_acc, epoch+1)
-                self.writer.add_scalar('val/val_overall_acc', val_overall_acc, epoch+1)
-                self.writer.add_scalar('val/val_loss', loss, epoch+1)
+                # self.writer.add_scalar('val/val_mean_class_acc', val_mean_class_acc, epoch+1)
+                self.writer.add_scalar('val/val_overall_acc', val_overall_acc, epoch + 1)
+                self.writer.add_scalar('val/val_loss', loss, epoch + 1)
 
             # save best model
             if val_overall_acc > best_acc:
                 best_acc = val_overall_acc
-                #self.model.save(self.log_dir, epoch)
-                torch.save(self.model.state_dict(), 
-                self.log_dir, 
-                    "model-{}.pt".format(str(epoch)))
- 
+                # self.model.save(self.log_dir, epoch)
+                torch.save(self.model.state_dict(),
+                           self.log_dir,
+                           "model-{}.pt".format(str(epoch)))
+
             # adjust learning rate manually
-            if epoch > 0 and (epoch+1) % 10 == 0:
+            if epoch > 0 and (epoch + 1) % 10 == 0:
                 for param_group in self.optimizer.param_groups:
                     param_group['lr'] = param_group['lr'] * 0.5
 
         # export scalar data to JSON for external processing
-        self.writer.export_scalars_to_json(self.log_dir+"/all_scalars.json")
+        self.writer.export_scalars_to_json(self.log_dir + "/all_scalars.json")
         self.writer.close()
 
     def update_validation_accuracy(self):
@@ -131,36 +131,39 @@ class GCNTrainer(object):
             all_correct_points += correct_points
             all_points += results.size()[0]
 
-        print ('Total # of test models: ', all_points)
-        #val_mean_class_acc = np.mean((samples_class - wrong_class) / samples_class)
+        print('Total # of test models: ', all_points)
+        # val_mean_class_acc = np.mean((samples_class - wrong_class) / samples_class)
         acc = all_correct_points.float() / all_points
         val_overall_acc = acc.cpu().data.numpy()
         loss = all_loss / len(self.val_loader)
 
-        #print ('val mean class acc. : ', val_mean_class_acc)
-        print ('val overall acc. : ', val_overall_acc)
-        print ('val loss : ', loss)
+        # print ('val mean class acc. : ', val_mean_class_acc)
+        print('val overall acc. : ', val_overall_acc)
+        print('val loss : ', loss)
 
         self.model.train()
 
-        #return loss, val_overall_acc, val_mean_class_acc
+        # return loss, val_overall_acc, val_mean_class_acc
         return loss, val_overall_acc
+
 
 if __name__ == '__main__':
     args = make_args()
 
     log_dir = '/home/project/gcn/APBGCN/log'
-    train_dataset = SkeletonDataset(root="/home/project/gcn/APBGCN", name='ntu_cs_train_test', use_motion_vector=False, benchmark='cs', sample = 'train')
-    valid_dataset = SkeletonDataset(root="/home/project/gcn/APBGCN", name='ntu_cs_val_test', use_motion_vector=False, benchmark='cs', sample = 'val')
-    
-    train_loader = DataLoader(train_dataset.data, batch_size = args.batch_size)
-    valid_loader = DataLoader(valid_dataset.data, batch_size = args.batch_size)
+    train_dataset = SkeletonDataset(root="/home/project/gcn/APBGCN", name='ntu_cs_train_test', use_motion_vector=False,
+                                    benchmark='cs', sample='train')
+    valid_dataset = SkeletonDataset(root="/home/project/gcn/APBGCN", name='ntu_cs_val_test', use_motion_vector=False,
+                                    benchmark='cs', sample='val')
 
-    model = DualGraphTransformer(in_channels = 3, hidden_channels = 16, out_channels = 16, num_layers = 4, num_heads = 8)
-    #optimizer = optim.Adam(model.parameters(), lr=args.lr, weight_decay=args.weight_decay, betas=(0.9, 0.98))
+    train_loader = DataLoader(train_dataset.data, batch_size=args.batch_size)
+    valid_loader = DataLoader(valid_dataset.data, batch_size=args.batch_size)
+
+    model = DualGraphTransformer(in_channels=3, hidden_channels=16, out_channels=16, num_layers=4, num_heads=8)
+    # optimizer = optim.Adam(model.parameters(), lr=args.lr, weight_decay=args.weight_decay, betas=(0.9, 0.98))
 
     noam_opt = get_std_opt(model, args)
 
-
-    trainer = GCNTrainer(model, train_loader, train_dataset.labels, valid_loader, valid_dataset.labels, train_dataset.skeleton_, noam_opt.optimizer, nn.CrossEntropyLoss(), log_dir)
+    trainer = GCNTrainer(model, train_loader, train_dataset.labels, valid_loader, valid_dataset.labels,
+                         train_dataset.skeleton_, noam_opt.optimizer, nn.CrossEntropyLoss(), log_dir)
     trainer.train(args.epoch_num)
