62def48d0bc222d1294af2e81dae36ec4d6a52df
imj2185 <dancingnum@gmail.com>
2021-03-02 23:31:56 -0500

    added 2 stream approach

---
+++ models/net2s.py
@@ -0,0 +1,131 @@
+from abc import ABC
+
+import torch
+import torch.nn as nn
+# from third_party.performer import SelfAttention
+from einops import rearrange
+
+
+from .attentions import SpatialEncoderLayer, TemporalEncoderLayer
+from models.positional_encoding import SeqPosEncoding
+from .layers import GlobalContextAttention
+from utility.tree import tree_encoding_from_traversal
+
+
+class DualGraphEncoder(nn.Module, ABC):
+    def __init__(self,
+                 in_channels,
+                 hidden_channels,
+                 out_channels,
+                 num_layers,
+                 num_heads=8,
+                 num_joints=25,
+                 classes=60,
+                 drop_rate=None,
+                 sequential=True,
+                 trainable_factor=False,
+                 num_conv_layers=3):
+        super(DualGraphEncoder, self).__init__()
+        if drop_rate is None:
+            self.drop_rate = [0.3, 0.3, 0.3, 0.3]
+        else:
+            self.drop_rate = drop_rate
+        self.spatial_factor = nn.Parameter(torch.ones(num_layers)) * 0.5
+        self.sequential = sequential
+        self.num_layers = num_layers
+        self.num_conv_layers = num_conv_layers
+        self.num_joints = num_joints
+        self.num_classes = classes
+        self.dropout = drop_rate
+        self.trainable_factor = trainable_factor
+        self.bn = nn.BatchNorm1d(hidden_channels * 25)
+        channels = [in_channels] + [hidden_channels] * (num_layers - 1) + [out_channels]
+        # channels = [hidden_channels * (i + 1)^2 for i in range(num_layers - 1)]
+        # self.spatial_norms = nn.ModuleList([
+        # nn.BatchNorm1d(channels[i + 1]) for i in range(num_layers)
+        #    nn.LayerNorm(channels[i + 1]) for i in range(num_layers)
+        # ])
+
+        channels_ = channels[1:] + [out_channels]
+
+        
+        self.tree_encoding = tree_encoding_from_traversal(onehot_length=3, max_padding=hidden_channels)
+        #self.embedding_weight = nn.Parameter(torch.randn(num_joints, hidden_channels))
+        self.positional_encoding = SeqPosEncoding(model_dim=hidden_channels)
+
+        # self.lls = nn.ModuleList([nn.Linear(in_features=channels[i],
+        #                                     out_features=channels[i + 1]) for i in range(num_layers)])
+        self.lls = nn.Linear(in_features=channels[0], out_features=channels[1])
+
+        self.spatial_layers = nn.ModuleList([
+            SpatialEncoderLayer(in_channels=channels_[i],
+                         mdl_channels=channels_[i + 1],
+                         heads=num_heads,
+                         dropout=drop_rate) for i in range(num_layers)])
+
+        self.temporal_layers = nn.ModuleList([
+            TemporalEncoderLayer(in_channels=channels_[i],
+                         mdl_channels=channels_[i + 1],
+                         heads=num_heads,
+                         dropout=drop_rate) for i in range(num_layers)])
+
+        """self.temporal_layers = nn.ModuleList([
+            # necessary parameters are: dim
+            EncoderLayer(in_channels=channels_[i],  # TODO ??? potential dimension problem
+                         mdl_channels=channels_[i + 1],
+                         spatial=False,
+                         heads=num_heads,
+                         dropout=drop_rate) for i in range(num_layers)])"""
+
+        self.context_attention = GlobalContextAttention(in_channels=out_channels)
+
+        self.mlp_head = nn.Sequential(
+            nn.LayerNorm(out_channels * num_joints),
+            nn.Linear(out_channels * num_joints, out_channels * num_joints),
+            # nn.Tanh(),
+            nn.LeakyReLU(),
+            nn.Linear(out_channels * num_joints, classes)
+        )
+
+    def forward(self, t, adj, bi):  # t: tensor, adj: dataset.skeleton_
+        """
+
+        :param t: tensor
+        :param adj: adjacency matrix (sparse)
+        :param bi: batch index
+        :return: tensor
+        """
+        """ if self.sequential:  # sequential architecture
+            for i in range(self.num_layers):
+                t = rearrange(fn.relu(self.spatial_layers[i](t, adj)),
+                              'b n c -> n b c')
+                t = rearrange(fn.relu(fn.layer_norm(fn.dropout(self.temporal_layers[i](t),
+                                                               self.dropout),
+                                                    t.shape[1:]) + t),  # residual and add_norm
+                              'n b c -> b n c')
+        else:  # parallel architecture"""
+        t = self.lls(t)
+        c = t.shape[-1]
+        t = self.bn(rearrange(t, 'b n c -> b (n c)'))
+        #t = rearrange(t, 'b (n c) -> b n c', c=c)
+        t = rearrange(t, 'b (n c) -> n b c', c=c)
+
+        t = self.positional_encoding(t, bi)
+        t = rearrange(t, 'n b c -> b n c')
+        
+        # Core pipeline
+        for i in range(self.num_layers):
+            u = t # branch
+            t = self.spatial_layers[i](t, adj, tree_encoding=self.tree_encoding)
+            u = rearrange(u, 'f n c -> n f c')
+            u = self.temporal_layers[i](u, bi)
+            u = rearrange(u, 'n f c -> f n c')
+            t = u + t
+
+        t = rearrange(t, 'f n c -> n f c')
+        # bi_ = bi[:bi.shape[0]:2**self.num_layers]
+        t = rearrange(self.context_attention(t, batch_index=bi),
+                      'n f c -> f (n c)')  # bi is the shrunk along the batch index
+        t = self.mlp_head(t)
+        # return fn.sigmoid(t)  # dimension (b, n, oc)
+        return t
---
+++ train2s.py
@@ -0,0 +1,275 @@
+import os
+import os.path as osp
+import time
+from random import shuffle
+
+import matplotlib
+matplotlib.use('Agg') 
+import matplotlib.pyplot as plt
+
+import numpy as np
+import torch
+import torch.nn as nn
+from tensorboardX import SummaryWriter
+from torch_geometric.data import DataLoader
+from tqdm import tqdm, trange
+
+from args import make_args
+from data.dataset3 import SkeletonDataset
+from models.net2s import DualGraphEncoder
+from optimizer import SGD_AGC, CosineAnnealingWarmupRestarts
+from utility.helper import make_checkpoint, load_checkpoint
+from random import shuffle
+#import imageio
+#import adamod
+
+def gif_grad_flow(path, gif_path, name):
+    with imageio.get_writer(osp.join(gif_path, name + '.gif'), mode='I') as writer:
+        for filename in path:
+            image = imageio.imread(filename)
+            writer.append_data(image)
+
+    for filename in path:
+        os.remove(filename)
+
+def plot_grad_flow(named_parameters, path, writer, step):
+    ave_grads = []
+    layers = []
+    empty_grads = []
+    # total_norm = 0
+    for n, p in named_parameters:
+        if p.requires_grad and not (("bias" in n) or ("norm" in n) or ("bn" in n) or ("gain" in n)):
+            if p.grad is not None:
+                # writer.add_scalar('gradients/' + n, p.grad.norm(2).item(), step)
+                # writer.add_histogram('gradients/' + n, p.grad, step)
+                # total_norm += p.grad.data.norm(2).item()
+                layers.append(n)
+                ave_grads.append(p.grad.abs().mean().cpu().item())
+            else:
+                empty_grads.append({n: p.mean().cpu().item()})
+    # total_norm = total_norm ** (1. / 2)
+    # print("Norm : ", total_norm)
+    #plt.tight_layout()
+    plt.plot(ave_grads, alpha=0.3, color="b")
+    plt.hlines(0, 0, len(ave_grads) + 1, linewidth=1.5, color="k")
+    plt.xticks(np.arange(0, len(ave_grads), 1), layers, rotation="vertical", fontsize=4)
+    plt.xlim(xmin=0, xmax=len(ave_grads))
+    plt.xlabel("Layers")
+    plt.ylabel("average gradient")
+    plt.title("Gradient flow" + str(step))
+    plt.grid(True)
+    plt.savefig(path, dpi=300)
+    #plt.close()
+    # plt.show()
+
+
+def run_epoch(data_loader,
+              model,
+              optimizer,
+              loss_compute,
+              dataset,
+              device,
+              is_train=True,
+              desc=None,
+              args=None,
+              writer=None,
+              epoch_num=0):
+    """Standard Training and Logging Function
+
+        :param data_loader:
+        :param model:
+        :param optimizer:
+        :param loss_compute:
+        :param dataset:
+        :param device:
+        :param is_train:
+        :param desc:
+        :param args:
+        :param writer:
+        :param epoch_num:
+
+    """
+    # torch.autograd.set_detect_anomaly(True)
+    running_loss = 0.
+    accuracy = 0.
+    correct = 0
+    total_samples = 0
+    start = time.time()
+    total_batch = len(dataset) // args.batch_size + 1
+    gradflow_file_list = []
+    for i, batch in tqdm(enumerate(data_loader),
+                         total=total_batch,
+                         desc=desc):
+        batch = batch.to(device)
+        sample, label, bi = batch.x, batch.y, batch.batch.to(device)
+
+        with torch.set_grad_enabled(is_train):
+            out = model(sample, adj=dataset.skeleton_.to(device), bi=bi)
+            loss = loss_compute(out, label.long())
+            loss_ = loss
+            if is_train:
+                l2_lambda = args.weight_decay
+                for param in model.parameters():
+                    if param.requires_grad:
+                        loss += l2_lambda * torch.sum(((param)) ** 2)
+                
+                optimizer.zero_grad()
+                loss.backward()
+                # torch.nn.utils.clip_grad_norm_(model.parameters(), 9.0)
+                optimizer.step()
+                if i % 400 == 0:
+                    step = (i + 1) + total_batch * epoch_num
+                    path = osp.join(os.getcwd(), 'gradflow')
+                    if not osp.exists(path):
+                        os.mkdir(path)
+                    plot_grad_flow(model.named_parameters(), osp.join(path, '%3d:%d.png' % (epoch_num, i)), writer, step)
+                    gradflow_file_list.append(osp.join(path, '%3d:%d.png' % (epoch_num, i)))
+
+                # plot_grad_flow(model.named_parameters(), writer, (i + 1) + total_batch * epoch_num)
+                # for name, param in model.named_parameters():
+                # if param.requires_grad and param.grad is not None:
+                # writer.add_scalar('gradients/' + name, param.grad.norm(2).item(), (i + 1) + total_batch * epoch_num)
+
+            # statistics
+            running_loss += loss_.item()
+            pred = torch.max(out, 1)[1]
+            total_samples += label.size(0)
+            correct += (pred == label).double().sum().item()
+    
+    gif_path = osp.join(os.getcwd(), 'gif_gradlow')
+    if not osp.exists(gif_path):
+        os.mkdir(gif_path)
+    #gif_grad_flow(gradflow_file_list, gif_path, str(epoch_num))
+    gradflow_file_list = []
+
+    elapsed = time.time() - start
+    accuracy = correct / total_samples * 100.
+    print('\n------ loss: %.3f; accuracy: %.3f; average time: %.4f' %
+          (running_loss / total_batch, accuracy, elapsed / len(dataset)))
+
+    return running_loss / total_batch, accuracy
+
+
+def main():
+    # torch.cuda.empty_cache()
+    args = make_args()
+    writer = SummaryWriter(args.log_dir)
+    #writer.add_hparams({'lr': args.lr, 
+    #                    'bsize': args.batch_size},
+    #                    {'hparam/num_enc_layers':args.num_enc_layers,
+    #                    'hparam/num_conv_layers': args.num_conv_layers,
+    #                    'hparam/temp_conv_drop': args.dropout[0], 
+    #                    'hparam/sparse_attention_drop': args.dropout[1],
+    #                    'hparam/add_norm_drop' : args.dropout[2], 
+    #                    'hparam/ffn_drop' : args.dropout[3], 
+    #                    'hparam/hid_channels': args.hid_channels
+    #                    })
+
+    device = torch.device('cuda:0') if args.use_gpu and torch.cuda.is_available() else torch.device('cpu')
+    # device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+    # download and save the dataset
+    train_ds = SkeletonDataset(args.dataset_root, name='ntu_60',
+                               use_motion_vector=False,
+                               benchmark='xsub', sample='train')
+    test_ds = SkeletonDataset(args.dataset_root, name='ntu_60',
+                              use_motion_vector=False,
+                              benchmark='xsub', sample='val')
+
+    last_train = int(len(train_ds) * 0.8)
+
+    # randomly split into around 80% train, 10% val and 10% train
+    # train_loader = DataLoader(train_ds.data,
+    #                          batch_size=args.batch_size,
+    #                          shuffle=True)
+    test_loader = DataLoader(test_ds,
+                             batch_size=args.batch_size,
+                             shuffle=True)
+
+    # criterion = LabelSmoothing(V, padding_idx=dataset.pad_id, smoothing=0.1)
+    # make_model black box
+    last_epoch = 0
+    model = DualGraphEncoder(in_channels=args.in_channels,
+                             hidden_channels=args.hid_channels,
+                             out_channels=args.out_channels,
+                             num_layers=args.num_enc_layers,
+                             num_heads=args.heads,
+                             sequential=False,
+                             num_conv_layers=args.num_conv_layers,
+                             drop_rate=args.drop_rate)
+    model = model.to(device)
+    print(sum(p.numel() for p in model.parameters()))
+    # noam_opt = get_std_opt(model, args)
+
+    optimizer = SGD_AGC(model.parameters(), lr=args.lr, momentum=0.9, weight_decay=args.weight_decay)
+    decay_rate = 0.97
+    lr_scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer=optimizer, gamma=decay_rate)
+    #lr_scheduler = CosineAnnealingWarmupRestarts(optimizer, first_cycle_steps=12, cycle_mult=1.0, max_lr=0.1,
+    #                                             min_lr=1e-4, warmup_steps=3, gamma=0.4)
+    if args.load_model:
+        last_epoch = args.load_epoch
+        last_epoch, loss = load_checkpoint(osp.join(args.save_root,
+                                                    args.save_name + '_' + str(last_epoch) + '.pickle'),
+                                           model, optimizer)
+        print("Load Model: ", last_epoch)
+
+    loss_compute = nn.CrossEntropyLoss().to(device)
+
+    for epoch in trange(last_epoch, args.epoch_num + last_epoch):
+        shuffled_list = [i for i in range(len(train_ds))]
+        shuffle(shuffled_list)
+        train_ds = train_ds[shuffled_list]
+
+        train_ds_ = train_ds[:last_train]
+        valid_ds_ = train_ds[last_train:]
+
+        train_loader = DataLoader(train_ds_,
+                                  batch_size=args.batch_size,
+                                  shuffle=True)
+        valid_loader = DataLoader(valid_ds_,
+                                  batch_size=args.batch_size,
+                                  shuffle=True)
+        # print('Epoch: {} Training...'.format(epoch))
+        model.train(True)
+        lr = optimizer.state_dict()['param_groups'][0]['lr']
+        writer.add_scalar('params/lr', lr, epoch)
+
+        loss, accuracy = run_epoch(train_loader, model, optimizer,
+                                   loss_compute, train_ds_, device, is_train=True,
+                                   desc="Train Epoch {}".format(epoch + 1), args=args, writer=writer, epoch_num=epoch)
+        print('Epoch: {} Evaluating...'.format(epoch + 1))
+
+        # TODO Save model
+        writer.add_scalar('train/train_loss', loss, epoch + 1)
+        writer.add_scalar('train/train_overall_acc', accuracy, epoch + 1)
+
+        if epoch % args.epoch_save == 0:
+            make_checkpoint(args.save_root, args.save_name, epoch, model, optimizer, loss)
+
+        # Validation
+        model.eval()
+        loss, accuracy = run_epoch(valid_loader, model, optimizer,
+                                   loss_compute, valid_ds_, device, is_train=False,
+                                   desc="Valid Epoch {}".format(epoch + 1), args=args, writer=writer, epoch_num=epoch)
+
+        writer.add_scalar('val/val_loss', loss, epoch + 1)
+        writer.add_scalar('val/val_overall_acc', accuracy, epoch + 1)
+
+        # if epoch > 15:
+        lr_scheduler.step()
+
+        if (epoch+1) % 5 == 0:
+            model.eval()
+            loss, accuracy = run_epoch(test_loader, model, optimizer,
+                                    loss_compute, test_ds, device, is_train=False,
+                                    desc="Final test: ", args=args, writer=writer, epoch_num=epoch)
+
+            writer.add_scalar('test/test_loss', loss, epoch + 1)
+            writer.add_scalar('test/test_overall_acc', accuracy, epoch + 1)
+
+    writer.export_scalars_to_json(osp.join(args.log_dir, "all_scalars.json"))
+    writer.close()
+
+
+if __name__ == "__main__":
+    main()
