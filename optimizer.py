1bdc7c67ac2df31c3d749c2065e1ccca3055ee60
Feng Shi <shi.feng@cs.ucla.edu>
2021-03-05 22:33:12 -0800

    add the annealed gaussian gradient noise to SGD_AGC

--- optimizer.py
+++ optimizer.py
@@ -59,7 +59,7 @@ def unitwise_norm(x: torch.Tensor):
     else:
         raise ValueError('Wrong input dimensions')
 
-    return torch.sum(x**2, dim=dim, keepdim=keepdim) ** 0.5
+    return torch.sum(x ** 2, dim=dim, keepdim=keepdim) ** 0.5
 
 
 class SGD_AGC(Optimizer):
@@ -106,7 +106,8 @@ class SGD_AGC(Optimizer):
     """
 
     def __init__(self, params, lr=required, momentum=0, dampening=0,
-                 weight_decay=0, nesterov=False, clipping=1e-2, eps=1e-3):
+                 weight_decay=0, nesterov=False, clipping=1e-2, eps=1e-3,
+                 eta=0.3, gamma=0.55):
         if lr is not required and lr < 0.0:
             raise ValueError("Invalid learning rate: {}".format(lr))
         if momentum < 0.0:
@@ -124,6 +125,9 @@ class SGD_AGC(Optimizer):
         if nesterov and (momentum <= 0 or dampening != 0):
             raise ValueError(
                 "Nesterov momentum requires a momentum and zero dampening")
+        self.t = 0
+        self.eta = eta
+        self.gamma = gamma
         super(SGD_AGC, self).__init__(params, defaults)
 
     def __setstate__(self, state):
@@ -152,12 +156,19 @@ class SGD_AGC(Optimizer):
                 grad_norm = unitwise_norm(p.grad.detach())
                 max_norm = param_norm * group['clipping']
 
-                trigger = grad_norm > max_norm   # TODO: not working if "grad_norm < max_norm"
+                trigger = grad_norm > max_norm  # TODO: not working if "grad_norm < max_norm"
 
                 clipped_grad = p.grad * \
-                    (max_norm / torch.max(grad_norm,
-                                          torch.tensor(1e-6).to(grad_norm.device)))
+                               (max_norm / torch.max(grad_norm,
+                                                     torch.tensor(1e-6).to(grad_norm.device)))
                 p.grad.detach().copy_(torch.where(trigger, clipped_grad, p.grad))
+
+                # add noise to gradients
+                normal = torch.empty(p.shape).normal_(
+                    mean=0, std=math.sqrt(self.eta / ((1 + self.t) ** self.gamma))).to(p.device)
+                p.grad += normal
+
+        self.t += 1
 
         for group in self.param_groups:
             weight_decay = group['weight_decay']
@@ -199,51 +210,53 @@ class CosineAnnealingWarmupRestarts(_LRScheduler):
         gamma(float): Decrease rate of max learning rate by cycle. Default: 1.
         last_epoch (int): The index of last epoch. Default: -1.
     """
-    
+
     def __init__(self,
-                 optimizer : torch.optim.Optimizer,
-                 first_cycle_steps : int,
-                 cycle_mult : float = 1.,
-                 max_lr : float = 0.1,
-                 min_lr : float = 0.001,
-                 warmup_steps : int = 0,
-                 gamma : float = 1.,
-                 last_epoch : int = -1
-        ):
+                 optimizer: torch.optim.Optimizer,
+                 first_cycle_steps: int,
+                 cycle_mult: float = 1.,
+                 max_lr: float = 0.1,
+                 min_lr: float = 0.001,
+                 warmup_steps: int = 0,
+                 gamma: float = 1.,
+                 last_epoch: int = -1
+                 ):
         assert warmup_steps < first_cycle_steps
-        
-        self.first_cycle_steps = first_cycle_steps # first cycle step size
-        self.cycle_mult = cycle_mult # cycle steps magnification
-        self.base_max_lr = max_lr # first max learning rate
-        self.max_lr = max_lr # max learning rate in the current cycle
-        self.min_lr = min_lr # min learning rate
-        self.warmup_steps = warmup_steps # warmup step size
-        self.gamma = gamma # decrease rate of max learning rate by cycle
-        
-        self.cur_cycle_steps = first_cycle_steps # first cycle step size
-        self.cycle = 0 # cycle count
-        self.step_in_cycle = last_epoch # step size of the current cycle
-        
+
+        self.first_cycle_steps = first_cycle_steps  # first cycle step size
+        self.cycle_mult = cycle_mult  # cycle steps magnification
+        self.base_max_lr = max_lr  # first max learning rate
+        self.max_lr = max_lr  # max learning rate in the current cycle
+        self.min_lr = min_lr  # min learning rate
+        self.warmup_steps = warmup_steps  # warmup step size
+        self.gamma = gamma  # decrease rate of max learning rate by cycle
+
+        self.cur_cycle_steps = first_cycle_steps  # first cycle step size
+        self.cycle = 0  # cycle count
+        self.step_in_cycle = last_epoch  # step size of the current cycle
+        self.last_epoch = 0
+
         super(CosineAnnealingWarmupRestarts, self).__init__(optimizer, last_epoch)
-        
+
         # set learning rate min_lr
         self.init_lr()
-    
+
     def init_lr(self):
         self.base_lrs = []
         for param_group in self.optimizer.param_groups:
             param_group['lr'] = self.min_lr
             self.base_lrs.append(self.min_lr)
-    
+
     def get_lr(self):
         if self.step_in_cycle == -1:
             return self.base_lrs
         elif self.step_in_cycle < self.warmup_steps:
-            return [(self.max_lr - base_lr)*self.step_in_cycle / self.warmup_steps + base_lr for base_lr in self.base_lrs]
+            return [(self.max_lr - base_lr) * self.step_in_cycle / self.warmup_steps +
+                    base_lr for base_lr in self.base_lrs]
         else:
             return [base_lr + (self.max_lr - base_lr) \
-                    * (1 + math.cos(math.pi * (self.step_in_cycle-self.warmup_steps) \
-                                    / (self.cur_cycle_steps - self.warmup_steps))) / 2
+                    * (1 + math.cos(math.pi * (self.step_in_cycle - self.warmup_steps) /
+                                    (self.cur_cycle_steps - self.warmup_steps))) / 2
                     for base_lr in self.base_lrs]
 
     def step(self, epoch=None):
@@ -253,7 +266,8 @@ class CosineAnnealingWarmupRestarts(_LRScheduler):
             if self.step_in_cycle >= self.cur_cycle_steps:
                 self.cycle += 1
                 self.step_in_cycle = self.step_in_cycle - self.cur_cycle_steps
-                self.cur_cycle_steps = int((self.cur_cycle_steps - self.warmup_steps) * self.cycle_mult) + self.warmup_steps
+                self.cur_cycle_steps = int((self.cur_cycle_steps - self.warmup_steps) *
+                                           self.cycle_mult) + self.warmup_steps
         else:
             if epoch >= self.first_cycle_steps:
                 if self.cycle_mult == 1.:
@@ -262,13 +276,14 @@ class CosineAnnealingWarmupRestarts(_LRScheduler):
                 else:
                     n = int(math.log((epoch / self.first_cycle_steps * (self.cycle_mult - 1) + 1), self.cycle_mult))
                     self.cycle = n
-                    self.step_in_cycle = epoch - int(self.first_cycle_steps * (self.cycle_mult ** n - 1) / (self.cycle_mult - 1))
-                    self.cur_cycle_steps = self.first_cycle_steps * self.cycle_mult ** (n)
+                    self.step_in_cycle = epoch - int(self.first_cycle_steps * (self.cycle_mult ** n - 1) /
+                                                     (self.cycle_mult - 1))
+                    self.cur_cycle_steps = self.first_cycle_steps * self.cycle_mult ** n
             else:
                 self.cur_cycle_steps = self.first_cycle_steps
                 self.step_in_cycle = epoch
-                
-        self.max_lr = self.base_max_lr * (self.gamma**self.cycle)
+
+        self.max_lr = self.base_max_lr * (self.gamma ** self.cycle)
         self.last_epoch = math.floor(epoch)
         for param_group, lr in zip(self.optimizer.param_groups, self.get_lr()):
             param_group['lr'] = lr
