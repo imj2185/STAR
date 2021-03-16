82790a10a8030bf793d40f69716a00e6c3a7b5b1
imj2185 <imj2185@gmail.com>
2021-03-16 19:55:07 -0400

    label smoothing added

--- optimizer.py
+++ optimizer.py
@@ -317,3 +317,16 @@ class MaxOneClipper(object):
         if hasattr(module, 'weight'):
             w = module.weight.data
             w = nn.functional.softmax(w, dim=-1)
+
+
+class LabelSmoothingCrossEntropy(nn.Module):
+    def __init__(self):
+        super(LabelSmoothingCrossEntropy, self).__init__()
+    def forward(self, x, target, smoothing=0.1):
+        confidence = 1. - smoothing
+        logprobs = nn.functional.log_softmax(x, dim=-1)
+        nll_loss = -logprobs.gather(dim=-1, index=target.unsqueeze(1))
+        nll_loss = nll_loss.squeeze(1)
+        smooth_loss = -logprobs.mean(dim=-1)
+        loss = confidence * nll_loss + smoothing * smooth_loss
+        return loss.mean()
