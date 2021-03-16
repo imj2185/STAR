b3616163445dd473b37098c225737c0d3efde241
Feng <shi.feng@cs.ucla.edu>
2021-03-16 00:04:35 -0700

    update dataset3.py

--- data/dataset3.py
+++ data/dataset3.py
@@ -52,7 +52,7 @@ def torch_rotation_matrix(axis, theta):
     Return the rotation matrix associated with counterclockwise rotation about
     the given axis by theta radians.
     """
-    if torch.abs(axis).sum() < 1e-6 or torch.abs(theta) < 1e-6:
+    if (torch.abs(axis).sum() < 1e-6) or (torch.abs(theta) < 1e-6):
         return torch.eye(3)
     # axis = axis.tolist()
     axis = axis / torch.sqrt(torch.dot(axis, axis))
@@ -87,59 +87,48 @@ def rotate_joints(data, joint_i, joint_j):
                                 torch.tensor([0, 0, 1],
                                              dtype=torch.float))
     rotate_mat = torch_rotation_matrix(axis, angle)
-
-    for i_f, frame in enumerate(data):
-        for i_j, joint in enumerate(frame):
-            try:
-                data[i_f, i_j] = torch.matmul(rotate_mat.float(), joint)
-            except RuntimeError:
-                print("double")
+    try:
+        data = torch.matmul(data, rotate_mat.float().transpose(1, 0))  # batched matrix multiplication
+    except RuntimeError:
+        print("double")
+    # for i_f, frame in enumerate(data):
+    #     for i_j, joint in enumerate(frame):
+    #         try:
+    #             data[i_f, i_j] = torch.matmul(rotate_mat.float(), joint)
+    #         except RuntimeError:
+    #             print("double")
 
 
 def pre_normalization(data, z_axis=None, x_axis=None):
-    # `index` of frames that have non-zero nodes
-    # if x_axis is None:
-    #    x_axis = [8, 4]
-    # if z_axis is None:
-    #    z_axis = [0, 1]
-    # debug_data = data.clone()
-    index = (data.sum(-1).sum(-1) != 0)
+    # data has features at the last dimension (NTU: x,y,z; Kinetics: x,y)
+    if z_axis is None:
+        z_axis = [0, 1]
+    if x_axis is None:
+        x_axis = [8, 4]
+
+    index = (data.sum(-1).sum(-1) != 0)  # index of non-zero frames
 
     if data.sum() == 0:
         print('empty video without skeleton information')
 
-    data = data[index]
-    # v1_frame = index[:(len(index) // 2)].sum().item()
-    # v2_frame = index[(len(index) // 2):].sum().item()
-
     # print('sub the center joint #1 (spine joint in ntu and neck joint in kinetics)')
     # Use the first person's body center (`1:2` along the nodes dimension)
-    # main_body_center = data[:v1_frame, 1:2, :].clone()
-    # For all `person`, compute the `mask` which is the non-zero channel dimension
-    # mask = rearrange((data.sum(-1) != 0), 'f n -> f n 1')
+    main_body_center = data[: data.shape[0] // 2, 1:2, :].clone()   # (F, V, C) where F = M * T
+    main_body_center = torch.cat([main_body_center] * 2)
+    data -= main_body_center
 
-    '''data[:v1_frame] = (data[:v1_frame] - main_body_center) * mask[:v1_frame]
-    if v1_frame > v2_frame:
-        data[v1_frame:] = (data[v1_frame:] - main_body_center[:v2_frame]) * mask[v1_frame:]
-    elif v1_frame < v2_frame:
-        reps = int(np.ceil(v2_frame / v1_frame))
-        pad = torch.cat([main_body_center for _ in range(reps)])[:v2_frame]
-        data[v1_frame:] = (data[v1_frame:] - pad) * mask[v1_frame:]
-    else:
-        data[v1_frame:] = (data[v1_frame:] - main_body_center) * mask[v1_frame:]'''
-
-    # print('parallel the bone between hip(jpt 0) and spine(jpt 1) of the first person to the z axis')
-    """joint_bottom = data[0, z_axis[0]]
+    # print('parallel the bone between hip (joint 0) and spine (joint 1) of the first person to the z axis')
+    joint_bottom = data[0, z_axis[0]]
     joint_top = data[0, z_axis[1]]
-    rotate_joints(data, joint_top, joint_bottom)"""
+    rotate_joints(data, joint_top, joint_bottom)
 
-    # print('parallel the bone between right shoulder(jpt 8) and
-    # left shoulder(jpt 4) of the first person to the x axis')
-    """joint_r_shoulder = data[0, x_axis[0]]
+    # print('parallel the bone between right shoulder(joint 8) and
+    # left shoulder(joint 4) of the first person to the x axis')
+    joint_r_shoulder = data[0, x_axis[0]]  # (F, N, C) ->
     joint_l_shoulder = data[0, x_axis[1]]
-    rotate_joints(data, joint_r_shoulder, joint_l_shoulder)"""
+    rotate_joints(data, joint_r_shoulder, joint_l_shoulder)
 
-    return data
+    return data[index]  # only non-zero frames are returned
 
 
 def read_skeleton_filter(file):
@@ -195,23 +184,28 @@ def num_processes():
     return os.cpu_count() - 2
 
 
+# NTU (A + A^2 + A^3)
+# [[0, 0, 0, 1, 1, 2, 2, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8,
+#   9, 9, 10, 10, 11, 11, 12, 12, 13, 13, 14, 14, 15, 16, 16, 17, 17, 18,
+#   18, 19, 20, 20, 20, 20, 21, 22, 22, 23, 24, 24],
+#  [1, 16, 12, 20, 0, 3, 20, 2, 20, 5, 4, 6, 7, 5, 22, 6, 20, 9,
+#   8, 10, 9, 11, 24, 10, 0, 13, 12, 14, 15, 13, 14, 0, 17, 16, 18, 17,
+#   19, 18, 8, 1, 4, 2, 22, 7, 21, 24, 11, 23]]
+
+# Kinetics (A + A^2 + A^3)
+# [[4, 3, 7, 6, 13, 12, 10, 9, 11, 8, 5, 2, 0, 15, 14, 17,
+#   16, 3, 2, 6, 5, 12, 11, 9, 8, 5, 2, 1, 1, 1, 0, 0, 15, 14],
+#  [3, 2, 6, 5, 12, 11, 9, 8, 5, 2, 1, 1, 1, 0, 0, 15, 14,
+#   4, 3, 7, 6, 13, 12, 10, 9, 11, 8, 5, 2, 0, 15, 14, 17, 16]]
+
+
 def skeleton_parts(num_joints=25, dataset='ntu', cat=True):
     if ('ntu' in dataset) or ('NTU' in dataset):
         sk_adj = torch.tensor(
-            # [[0, 0, 0, 1, 1, 2, 2, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8,
-            #   9, 9, 10, 10, 11, 11, 12, 12, 13, 13, 14, 14, 15, 16, 16, 17, 17, 18,
-            #   18, 19, 20, 20, 20, 20, 21, 22, 22, 23, 24, 24],
-            #  [1, 16, 12, 20, 0, 3, 20, 2, 20, 5, 4, 6, 7, 5, 22, 6, 20, 9,
-            #   8, 10, 9, 11, 24, 10, 0, 13, 12, 14, 15, 13, 14, 0, 17, 16, 18, 17,
-            #   19, 18, 8, 1, 4, 2, 22, 7, 21, 24, 11, 23]]
             [[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 21, 22, 23, 24],
              [1, 20, 20, 2, 20, 4, 5, 6, 20, 8, 9, 10, 0, 12, 13, 14, 0, 16, 17, 18, 22, 7, 24, 11]])
     elif 'kinetics' in dataset:
         sk_adj = torch.tensor(
-            # [[4, 3, 7, 6, 13, 12, 10, 9, 11, 8, 5, 2, 0, 15, 14, 17,
-            #   16, 3, 2, 6, 5, 12, 11, 9, 8, 5, 2, 1, 1, 1, 0, 0, 15, 14],
-            #  [3, 2, 6, 5, 12, 11, 9, 8, 5, 2, 1, 1, 1, 0, 0, 15, 14,
-            #   4, 3, 7, 6, 13, 12, 10, 9, 11, 8, 5, 2, 0, 15, 14, 17, 16]]
             [[0, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17],
              [1, 1, 2, 3, 1, 5, 6, 2, 8, 9, 5, 11, 12, 0, 0, 14, 15]]
         )
@@ -222,7 +216,6 @@ def skeleton_parts(num_joints=25, dataset='ntu', cat=True):
         return sk_adj
 
     sk_adj_undirected = torch.cat((sk_adj, torch.stack([sk_adj[1], sk_adj[0]])), dim=1)
-
     _, idx = sk_adj_undirected[0].sort()
     sk_adj_undirected = sk_adj_undirected[:, idx]
 
