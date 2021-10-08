2262e728ca10090e5ab8421bcf6946d3107c0c06
Feng Shi <shi.feng@cs.ucla.edu>
2021-10-08 18:04:34 +0800

    visualize skeleton

--- utility/__init__.py
+++ utility/__init__.py
@@ -1,4 +1,4 @@
 from .argparser import Parser
 from .timer import Timer
-from linalg import power_adj, spmm_
+from .linalg import power_adj, spmm_
 __all__ = ['Parser', 'Timer', 'power_adj', 'spmm_']
